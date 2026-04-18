af55c2d19c6e47e7de041138edeeab958a4ca643
ComposeDemo <danil.lapshin.2000@mail.ru>
2026-04-18 20:49:08 +0700

    grey

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.photogallery.ui.theme.PhotoGalleryTheme
 
@@ -29,10 +33,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PhotoGalleryScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Photo Gallery Screen",
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color.Gray),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Photo Gallery Screen")
+    }
 }
 
 @Preview(showBackground = true)
